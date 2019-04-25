b0daa720f188eb708ac492a54e1f1c7aac14494e
Alexander Simko <alexander.ch.simko@gmail.com>
2019-04-25 13:54:14 +0200

    Created working web server

--- server.py
+++ server.py
@@ -1,23 +1,4 @@
-#! /usr/bin/env python3
+from app import app
 
-import asyncio
-import websockets
-from collections import defaultdict
-
-connections = defaultdict(set)
-
-async def server(websocket, path):
-    connections[path].add(websocket)
-    print(f"Connection recived: {websocket}")
-    async for message in websocket:
-        for connection in connections[path]:
-            print(f"Sending {message} to {connection}")
-            await connection.send(message)
-    
-    print(f"Connection closed {websocket}")
-    connections[path].remove(websocket)
-
-start_server = websockets.serve(server, 'localhost', 1234)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == '__main__':
+    app.run()
